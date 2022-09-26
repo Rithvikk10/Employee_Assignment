084b6af0222b1d6fa6b56ba7a5d95d27cad2a3bb
Rithvik Vanteru <rithvik@Rithviks-MacBook-Pro.local>
2022-09-26 10:48:53 -0400

    control panel created

--- src/design/mainJFrame.java
+++ src/design/mainJFrame.java
@@ -1,2 +1,14 @@
-package design;public class mainJFrame {
+package design;
+
+import javax.swing.*;
+
+public class mainJFrame {
+    private JPanel mainPanel;
+    private JSplitPane splitPane;
+    private JPanel controlPanel;
+    private JPanel workArea;
+    private JButton create;
+    private JButton read;
+    private JButton update;
+    private JButton delete;
 }
--- src/model/Employee.java
+++ src/model/Employee.java
@@ -1,5 +1,104 @@
 package model;
 
-public class Product {
-    
+public class Employee {
+    private String name;
+    private String age;
+    private String employeeID;
+    private String gender;
+    private String startDate;
+    private String level;
+    private String teamInformation;
+    private String positionTitle;
+    private String phoneNumber;
+    private String email;
+    private String photo;
+
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(String startDate) {
+        this.startDate = startDate;
+    }
+
+    public String getLevel() {
+        return level;
+    }
+
+    public void setLevel(String level) {
+        this.level = level;
+    }
+
+    public String getTeamInformation() {
+        return teamInformation;
+    }
+
+    public void setTeamInformation(String teamInformation) {
+        this.teamInformation = teamInformation;
+    }
+
+    public String getPositionTitle() {
+        return positionTitle;
+    }
+
+    public void setPositionTitle(String positionTitle) {
+        this.positionTitle = positionTitle;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
 }
